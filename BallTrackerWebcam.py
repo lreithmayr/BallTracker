b8d07698b6fbc2fa9482ca0b45d76d87a84bf4aa
Lorenz <lorenz.reithmayr@t-online.de>
2021-05-02 13:41:04 +0200

    Refactored tracker into class

--- BallTrackerWebcam.py
+++ BallTrackerWebcam.py
@@ -6,68 +6,62 @@ import imutils
 from collections import deque
 
 
-def track_contours(frame, pts):
+class ContourTracker(object):
 
-    lt = np.array([65, 33, 71])
-    ut = np.array([90, 176, 255])
+    def __init__(self):
+        self.pts = deque(maxlen=64)
 
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lt, ut)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    def track_contours(self, cam_frame):
+        lt = np.array([65, 33, 71])
+        ut = np.array([90, 176, 255])
 
-    cntrs = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntrs = imutils.grab_contours(cntrs)
-    center = None
+        blurred = cv2.GaussianBlur(cam_frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lt, ut)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
-    if len(cntrs) > 0:
-        print("Works")
-        c = max(cntrs, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        cntrs = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cntrs = imutils.grab_contours(cntrs)
+        if len(cntrs) > 0:
+            c = max(cntrs, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            moments = cv2.moments(c)
+            center = (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
+            self.pts.appendleft(center)
 
-        if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-    else:
-        print ("Fail")
+            if radius > 10:
+                cv2.circle(cam_frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(cam_frame, center, 5, (0, 0, 255), -1)
 
-    pts.appendleft(center)
+        for i in range(1, len(self.pts)):
+            if self.pts[i - 1] is None or self.pts[i] is None:
+                continue
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(cam_frame, self.pts[i - 1], self.pts[i], (0, 255, 0), thickness)
 
-    for i in range(1, len(pts)):
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 255, 0), thickness)
+        return cam_frame
 
-    return frame, pts
-
-
-def plot_track(pts):
-    for pt in pts:
-        y_neg = np.negative(pt[1])
-        plt.scatter(pt[0], y_neg)
-    plt.show()
+    def plot_track(self):
+        for pt in self.pts:
+            y_neg = np.negative(pt[1])
+            plt.scatter(pt[0], y_neg)
+        plt.show()
 
 
 if __name__ == "__main__":
     vid = "http://192.168.0.94:8080/video"
-    pts = deque(maxlen=64)
-
     cap = cv2.VideoCapture(vid)
+    tracker = ContourTracker()
 
     while True:
         check, frame = cap.read()
-        frame = imutils.resize(frame, int(1920/1.5), int(1080/1.5))
-        frame, pts = track_contours(frame, pts)
-        cv2.imshow("Frame", frame)
+        frame = imutils.resize(frame, int(1920 / 1.5), int(1080 / 1.5))
+        tracked_frame = tracker.track_contours(frame)
+        cv2.imshow("Frame", tracked_frame)
         if cv2.waitKey(1) == 27:
             break
     cap.release()
     cv2.destroyAllWindows()
 
-    print(pts)
-
-    plot_track(pts)
+    tracker.plot_track()
