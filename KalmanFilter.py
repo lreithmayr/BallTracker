ed905a43b3343647678aa192ef3f11eddbb72b99
psychedelic-tortilla <63908215+psychedelic-tortilla@users.noreply.github.com>
2021-05-12 09:49:36 +0200

    Added process and measurement noise covariance matrices to KF

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -8,7 +8,8 @@ class KalmanFilter(object):
         self.kf = cv2.KalmanFilter(4, 2)
         self.kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
         self.kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
-        self.kf.processNoiseCov = 3e5* np.eye(4).astype(np.float32)
+        self.kf.processNoiseCov = 3e5 * np.eye(4).astype(np.float32)
+        self.kf.measurementNoiseCov = 1e-2 * np.eye(2).astype(np.float32)
 
     def estimate_position(self, pos_x, pos_y):
         measured = np.array([[np.float32(pos_x)], [np.float32(pos_y)]])
