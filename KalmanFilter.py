62d817c2ea844d62c85e999e7a672d472f92cb8f
Lorenz Reithmayr <lLorenz.reithmayr@t-online.degit config --global user.email lLorenz.reithmayr@t-online.de>
2021-05-13 17:01:53 +0200

    Implemented error covariance matrix in KF and drawing of error estimation on frame.

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -11,25 +11,25 @@ class KalmanFilter(object):
         self.kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
         self.kf.processNoiseCov = 3e5 * np.eye(4).astype(np.float32)
         self.kf.measurementNoiseCov = 1e-3 * np.eye(2).astype(np.float32)
-        self.kf.errorCovPost = np.diag([10, 10, 10, 10])**2
+        self.kf.errorCovPost = np.diag([1, 1, 1, 1])**2
         self.caller_name = inspect.stack()[1][0].f_code.co_filename
+        self.predicted = None
 
     def estimate_position(self, pos_x, pos_y, cam_frame, radius=None):
         measured = np.array([[np.float32(pos_x)], [np.float32(pos_y)]])
         self.kf.correct(measured)
-        predicted = self.kf.predict()
+        self.predicted = self.kf.predict()
         if "Video" in self.caller_name:
-            cv2.circle(cam_frame, (predicted[0], predicted[1]), 15, [0, 45, 255], 2, 8)
+            cv2.circle(cam_frame, (self.predicted[0], self.predicted[1]), 15, [0, 45, 255], 2, 8)
         elif "Webcam" in self.caller_name and radius is not None:
-            cv2.circle(cam_frame, (predicted[0], predicted[1]), int(radius), [0, 45, 255], 2, 8)
-        return predicted
+            cv2.circle(cam_frame, (self.predicted[0], self.predicted[1]), int(radius), [0, 45, 255], 2, 8)
+        return self.predicted
 
     def get_error_cov_pos(self):
-        print(self.kf.errorCovPost)
         return self.kf.errorCovPost
 
-#    def draw_estimation_error(self):
-        # err_x = 2 * np.sqrt(kf.get_errorCovPos()[0][0])
-        # err_y = 2 * np.sqrt(kf.get_errorCovPos()[1][1])
-        # uncert = (err_x + err_y) / 2
-        # cv2.circle(frame, (pred_pos[0], pred_pos[1]), int(uncert), [0, 45, 255], 2, 8)
+    def draw_estimation_error(self, frame):
+        err_x = 2 * np.sqrt(self.get_error_cov_pos()[0][0])
+        err_y = 2 * np.sqrt(self.get_error_cov_pos()[1][1])
+        uncert = (err_x + err_y) / 2
+        cv2.circle(frame, (self.predicted[0], self.predicted[1]), int(uncert), [0, 45, 255], 2, 8)
