33fe94b5ad46c44b8b71e811abe996086345177a
Lorenz Reithmayr <lLorenz.reithmayr@t-online.degit config --global user.email lLorenz.reithmayr@t-online.de>
2021-05-17 14:50:12 +0200

    KF from scratch outline

--- KFFromScratch.py
+++ KFFromScratch.py
@@ -21,10 +21,10 @@ class KFFromScratch(object):
 
     Gives:
 
-        x(k) = [1 0 dt 0    * [pos_x(k-1)    +   [0    *  [a_x(k)
-                0 1 0 dt       pos_y(k-1)         0        a_y(k)]
-                0 0 1 0        vel_x(k-1)         1
-                0 0 0 1]       vel_y(k-1)]        1]
+               [1 0 dt 0      [pos_x(k-1)        [0.5 * dt^2    0
+                0 1 0 dt       pos_y(k-1)          0        0.5 * dt^2          [a_x(k)
+        x(k) =  0 0 1 0    *   vel_x(k-1)    +     dt           0         *      a_y(k)]
+                0 0 0 1]       vel_y(k-1)]         0           dt]
 
 
 
