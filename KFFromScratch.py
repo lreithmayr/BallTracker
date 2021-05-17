535aad378c8775f949ce82038b4a3e163efcff9a
psychedelic-tortilla <63908215+psychedelic-tortilla@users.noreply.github.com>
2021-05-17 14:34:14 +0200

    Begun implementing KF from scratch

---
+++ KFFromScratch.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class KFFromScratch(object):
+
+    """"
+    Nomenclature from Wikipedia article on Kalman filters.
+
+    State vector:
+
+    x(k) = [pos_x(k)
+            pos_y(k)
+            vel_x(k)
+            vel_y(k)]
+
+    Transition model:
+
+        pos_x(k) = pos_x(k-1) + (vel_x(k-1) * dt) + 0.5 * dt^2 * a_x(k-1)
+        pos_y(k) = pos_y(k-1) + (vel_y(k-1) * dt) + 0.5 * dt^2 * a_y(k)
+        vel_x(k) = vel_x(k-1) + dt * a_x(k)
+        vel_y(k) = vel_y(k-1) + dt * a_y(k)
+
+    Gives:
+
+        x(k) = [1 0 dt 0    * [pos_x(k-1)    +   [0    *  [a_x(k)
+                0 1 0 dt       pos_y(k-1)         0        a_y(k)]
+                0 0 1 0        vel_x(k-1)         1
+                0 0 0 1]       vel_y(k-1)]        1]
+
+
+
+
+
+    """
+
+    def __init__(self):
+        self.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]])
